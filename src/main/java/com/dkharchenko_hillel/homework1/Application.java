8595bb201c261d7d45483664b605f1fdb283272f
d.kharchenko <everrrr4@gmail.com>
2022-08-25 20:53:26 +0300

    Simple Spring IoC application with XML-configuration is done

--- src/main/java/com/dkharchenko_hillel/homework1/Application.java
+++ src/main/java/com/dkharchenko_hillel/homework1/Application.java
@@ -7,9 +7,9 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         NameVerificationService nameVerificationService = (NameVerificationService) context.getBean("nameVerificationService");
-        QuizService questioningService = (QuizService) context.getBean("questioningService");
+        QuizService quizService = (QuizService) context.getBean("quizService");
 
         nameVerificationService.askNameAndLastname();
-        questioningService.executeQuiz();
+        quizService.executeQuiz();
     }
 }
